1a00f194b2100322710b5d28175e1e8c80e8a105
Dan Gray <dan@sinensis.co.uk>
2016-03-15 15:40:46 +0000

    JSON and schema changes

--- js/adapt-contrib-hotgraphic-audio.js
+++ js/adapt-contrib-hotgraphic-audio.js
@@ -16,7 +16,7 @@ define(function(require) {
             if (Adapt.device.screenSize == 'large') {
                 this.render();
 
-                if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+                if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                     this.replaceText(Adapt.audio.textSize);
                 }
             } else {
@@ -322,7 +322,7 @@ define(function(require) {
             var popupObject_body = itemModel.body;
 
             // If reduced text is enabled and selected
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.audio.textSize == 1) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.audio.textSize == 1) {
                 popupObject_title = itemModel.titleReduced;
                 popupObject_body = itemModel.bodyReduced;
             }
@@ -398,7 +398,7 @@ define(function(require) {
             var popupObject_body = itemModel.body;
 
             // If reduced text is enabled and selected
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.audio.textSize == 1) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.audio.textSize == 1) {
                 popupObject_title = itemModel.titleReduced;
                 popupObject_body = itemModel.bodyReduced;
             }
@@ -442,14 +442,22 @@ define(function(require) {
         // Reduced text
         replaceText: function(value) {
             // If enabled
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 // Change component title and body
                 if(value == 0) {
-                    this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
-                    this.$('.component-body-inner').html(this.model.get('body')).a11y_text();
+                    if (this.model.get('displayTitle')) {
+                        this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
+                    }
+                    if (this.model.get('body')) {
+                        this.$('.component-body-inner').html(this.model.get('body')).a11y_text();
+                    }
                 } else {
-                    this.$('.component-title-inner').html(this.model.get('displayTitleReduced')).a11y_text();
-                    this.$('.component-body-inner').html(this.model.get('bodyReduced')).a11y_text();
+                    if (this.model.get('displayTitleReduced')) {
+                        this.$('.component-title-inner').html(this.model.get('displayTitleReduced')).a11y_text();
+                    }
+                    if (this.model.get('bodyReduced')) {
+                        this.$('.component-body-inner').html(this.model.get('bodyReduced')).a11y_text();
+                    }
                 }
                 // Change each items title and body
                 for (var i = 0; i < this.model.get('_items').length; i++) {
